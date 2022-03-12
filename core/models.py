22162c0b7b350f0379793f03aae6f96b117a611e
amkhasanov <ainur0225@gmail.com>
2022-03-12 13:32:37 +0500

    Добавили BootstrapÐ, создали загатовку под проект

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField('Название', max_length=128)
     pages = models.IntegerField('Количество страниц', blank=True, null=True)
+    author = models.CharField('Имя автора', null=True, max_length=100)
 
     def __str__(self):
         return self.name
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Template, Context
+from .models import Book
 
 def hello(request):
-    return render(request, 'core/hello.html', {'name':'Vasya'})
+    return render(request, 'core/hello.html', {'books' : Book.objects.all()})
